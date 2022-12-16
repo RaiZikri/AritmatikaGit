0db92b3e23e8d2eb02da569fce77ee6f1b75d306
RaiZikri <rai.dzikri.n999@gmail.com>
2022-12-16 08:33:34 +0700

    Ini adalah inisialisasi project aritmatika

---
+++ src/main/java/com/mycompany/aritmatikaRai/Aritmatika.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aritmatikaRai;
+
+/**
+ *
+ * @author Raidz
+ */
+public class Aritmatika {
+   private double bilSatu;
+   private double bilDua;
+
+    public Aritmatika() {
+    }
+
+    public Aritmatika(double bilSatu, double bilDua) {
+        this.bilSatu = bilSatu;
+        this.bilDua = bilDua;
+    }
+
+    public double getBilSatu() {
+        return bilSatu;
+    }
+
+    public void setBilSatu(double bilSatu) {
+        this.bilSatu = bilSatu;
+    }
+
+    public double getBilDua() {
+        return bilDua;
+    }
+
+    public void setBilDua(double bilDua) {
+        this.bilDua = bilDua;
+    }
+   
+   /* Penjumlahan */
+    public double penjumlahan(){
+        return 0;
+    }
+   /* Pengurangan */
+    public double pengurangan(){
+        return 0;
+    }
+    
+   /* Perkalian */
+    public double perkalian(){
+        return 0;
+    }
+    
+   /* Pembagian */
+    public double pembagian(){
+        return 0;
+    }
+    
+}
